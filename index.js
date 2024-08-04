70340ce16a7630a3ab00409bbc1c0a6f9a86bf9d
Srikar7362 <149218927+Srikar7362@users.noreply.github.com>
2024-08-05 05:27:35 +0530

    Portfolio
    personal portfolio website

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector(".github").addEventListener("click",handleclick);
+function handleclick(){
+    window.location.href = "https://github.com/Srikar7362";
+}
+document.querySelector(".whatsapp").addEventListener("click",handleclick1);
+function handleclick1(){
+    window.location.href="https://web.whatsapp.com/"
+}
+document.querySelector(".linkdin").addEventListener("click",handleclick2);
+function handleclick2(){
+    window.location.href="http://linkedin.com/in/sai-srikar-517032255";
+}
